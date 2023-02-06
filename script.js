4daa016e8bafa74c7f37681f4b87c58d1a14a9c4
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-07 01:07:45 +0300

    Приоритет математических операций в JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-let a = 10;
-let b = 5;
-let c = a - b; // запишем разницу в переменную c
-let d = 7;
-let result = c + d;
-alert(result);      // выведет result
+let a = 2 * (8 / 4);
+alert(a); //выведет 4
