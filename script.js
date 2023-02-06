1b5c89c4ab5808c6d3df98455aa58bf0659dab72
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-07 02:17:23 +0300

    Получение символов строки на JavaScript

--- script.js
+++ script.js
@@ -1,62 +1,13 @@
-let test = Boolean(3);
-alert(test); //выведет true
+let str = 'abcde';
 
-let test1 = Boolean(0);
-alert(test1); //выведет false
+alert(str[0]); // выведет 'a'
+alert(str[2]); // выведет 'c'
+alert(str[4]); // выведет 'e'
 
-let test2 = Boolean(-1);
-alert(test2); //выведет true
+let o = str[4]+ str[3]+ str[2]+ str[1]+ str[0];
+alert(o);// строка наоборот 
 
-let test3 = Boolean(-0);
-alert(test3); //выведет false
+let str = 'abcde';
+let num = 3; 
 
-let test4 = Boolean(+0);
-alert(test4); //выведет false
-
-let test5 = Boolean('abc');
-alert(test5); //выведет true
-
-let test6 = Boolean('');
-alert(test6); //выведет false
-
-let test7 = Boolean('0');
-alert(test7); //выведет true
-
-let test8 = Boolean(true);
-alert(test8); //выведет true
-
-let test9 = Boolean(false);
-alert(test9); //выведет false
-
-let test10 = Boolean('true');
-alert(test10); //выведет true
-
-let test11 = Boolean('false');
-alert(test11); //выведет true
-
-let test12 = Boolean(null);
-alert(test12); //выведет false
-
-let test13 = Boolean('null');
-alert(test13); //выведет true
-
-let test14 = Boolean(undefined);
-alert(test14); //выведет false
-
-let test15 = Boolean('undefined');
-alert(test15); //выведет true
-
-let test16 = Boolean(NaN);
-alert(test16); //выведет false
-
-let test17 = Boolean('NaN');
-alert(test17); //выведет true
-
-let test18 = Boolean(3 * 'abc');
-alert(test18); //выведет false
-
-let test19 = Boolean(Infinity);
-alert(test19); //выведет true
-
-let test20 = Boolean(1 / 0);
-alert(test20); //выведет true
+alert(str[num]); // выведет символ номер которого в num
