1ccb42829f2c8559396d6c18f513c5557dbe60c8
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-07 01:52:00 +0300

    Выделение чисел в JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-let a = 2;
-let b = 3;
-alert(a + b);
+let num = parseInt('5px');
+let num1 = parseInt('6px');
+alert(num +num1);
+
+let Num = parseFloat('5.5px');
+let Num1 = parseFloat('6.25px');
+alert(Num + Num1 + 'px');
