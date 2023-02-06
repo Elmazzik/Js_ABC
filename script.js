9de3f9b6b0893bfe2ff5cd8d1c78f24f1293d101
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-07 01:05:22 +0300

    Математические операции с переменными в JavaScript

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-let a = 111 + 222 + 333;
-alert(a); // выведет 666
+let a = 10;
+let b = 5;
+let c = a - b; // запишем разницу в переменную c
+let d = 7;
+let result = c + d;
+alert(result);      // выведет result
