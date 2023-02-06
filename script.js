94a6b502ef388ad3dfcb107a727d9af9a083771d
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-07 01:31:21 +0300

    Значения Infinity и -Infinity

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-let str = `a
-b
-c`
-alert(str);
+let a;
+alert(a);
+
+let b = null;
+alert(b); 
+
+let c = true;
+alert(c); 
+
+let d = false;
+alert(d); 
+
+alert('abc' * 3); 
+
+alert( 10 / 0); // выведет Infinity
+alert(-10 / 0); // выведет -Infinity
