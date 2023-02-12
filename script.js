d8a56e44fc1ce98080120f14d11eca7927710af7
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-13 00:55:48 +0300

    Сокращенные операции в JavaScript

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-let Atest = '12345';
-alert(Atest[0] + Atest[1]);
-
-let test = String(12345); // строка
-alert(Number(test[0]) + Number(test[1]) + Number(test[2]) + Number(test[3]) + Number(test[4]));
-
-let Btest = String(12345); // строка
-alert(Btest[1] * Btest[2] * Btest[3] * Btest[4] * Btest[0]);
-
-let Ctest = '12345';
-alert(Ctest[4] + Ctest[3] + Ctest[2] + Ctest[1] + Ctest[0]);
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num);
