855198b684aadeb7b719117456313db13910c08d
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-13 00:50:52 +0300

    Строки с цифрами в JavaScript

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let str = 'abcde';
-let last = str.length - 1; // номер последнего символа
-alert(str[last]);
+let Atest = '12345';
+alert(Atest[0] + Atest[1]);
 
-let last2 = str.length - 2; // номер предпоследнего символа
-alert(str[last2]);
+let test = String(12345); // строка
+alert(Number(test[0]) + Number(test[1]) + Number(test[2]) + Number(test[3]) + Number(test[4]));
 
-let last3 = str.length - 3; // номер предпредпоследнего символа
-alert(str[last3]);
+let Btest = String(12345); // строка
+alert(Btest[1] * Btest[2] * Btest[3] * Btest[4] * Btest[0]);
+
+let Ctest = '12345';
+alert(Ctest[4] + Ctest[3] + Ctest[2] + Ctest[1] + Ctest[0]);
