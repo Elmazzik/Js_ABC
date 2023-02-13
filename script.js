1702977f45fd01b9812b79a402ea50bee3be4a92
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-13 23:14:50 +0300

    Поиск ошибок в коде

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
-document.write('Котики захватят мир!<br>');
+let num1 = 1;
+let num2 = 2;
+alert('сумма: ' + num1 + num2); 
 
-document.write('<i>Котики захватят мир!</i><br>');
+let a = 1;
+let b = 2;
+alert(a + b);
 
-let str = 'тут курсив';
-document.write('<i>' + str + '</i>');
-document.write('<br>');
+let num = '123';
+let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
+alert(sum);
 
-document.write('1<br>');
-document.write('2<br>');
-document.write('3<br>');
-document.write('4<br>');
-document.write('5<br>');
+let num3 = '123';
+alert(num3[0]);
+
+let plus = 0;
+alert(++plus);
+
+let m = '123';
+alert(m.length);
+
+let so = 24 / 60 / 60;
+alert(so.toFixed(4));
+
+let box = 123;
+let str = String(box);
+alert(str.length);
+
+let abc = 123;
+let str1 = String(abc);
+alert(str1[str1.length-1]);
+
+let nom = 123;
+let set = String(nom);
+alert(set.length);  
+
+let x = '123';
+let y = '456';
+let z = Number(x) + Number(y);
+alert(z);
+
+let aaa = 1;
+let bbb = 2;
+let ccc = 3;
+alert(Number(aaa + bbb + ccc));
