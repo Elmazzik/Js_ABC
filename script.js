8d49d129fdf1d81077aad5f40ac7a1e6414277b8
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-15 10:20:12 +0300

    Объекты в JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-let arr1 = [1, 2, 3, 4, 5];
-console.log(arr1[arr1.length-1]);
-
-let arr = [1, 2, 3, 4, 5];
-console.log(arr[1] + arr[2] + arr[3] + arr[4] + arr[0]);
-
-let arr2 = [1, 2, 3, 4, 5];
-console.log(arr2.length);
+let obj = {1: 'monday', 2: 'tuesday', 3: 'wednesday', 4: 'thursday', 5: 'friday', 6: 'saturday', 7: 'sunday'};
+console.log(obj);
