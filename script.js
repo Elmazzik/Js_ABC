037b997fae5407e8fa1140a9fd4fab25af261800
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-15 08:21:55 +0300

    Длина массива в JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let arr = [1, 2, 3];
-console.log(arr);
-
-let arr1 = ['a', 'b', 'c'];
-console.log(arr1);
+let arr = ['elmaz1', 'elmaz2', 'elmaz3'];
+console.log(arr.length) 
+console.log(arr[arr.length - 1])
