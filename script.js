db0249b25a458c06eeb87676a732e2d7414b30bf
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-15 08:54:39 +0300

    Изменение элементов массива в JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 let arr = ['a', 'b', 'c'];
-console.log(arr.length)
+arr[0] = '1';
+arr[1] = '2';
+arr[2] = '3';
+console.log(arr);
 
-let arr1 = [1, 2, 3];
-console.log(arr1[arr1.length - 1])
+let arr1 = ['1', '2', '3'];
+arr1[0] += 3;
+arr1[1] += 3;
+arr1[2] += 3;
+console.log(arr1);
+
+let arr3 = [1, 2, 3];
+arr3[0]++;
+arr3[1]++;
+arr3[2]++;
+console.log(arr3);
