1c445ff31a94c349ed60ac658b318cf573e68e42
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-15 09:21:53 +0300

    Поиск ошибок в коде с массивами JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-let arr = ['a', 'b', 'c', 'd', 'e'];
-delete arr[1];
-console.log(arr);
+let arr1 = [1, 2, 3, 4, 5];
+console.log(arr1[arr1.length-1]);
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr[1] + arr[2] + arr[3] + arr[4] + arr[0]);
+
+let arr2 = [1, 2, 3, 4, 5];
+console.log(arr2.length);
