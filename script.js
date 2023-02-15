837a7dd4002cc39cea321a23928843f2e3a6229f
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-15 08:56:13 +0300

    Добавление элементов по ключам JavaScript

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-let arr = ['a', 'b', 'c'];
-arr[0] = '1';
-arr[1] = '2';
-arr[2] = '3';
+let arr = [1, 2, 3];
+arr[3] = '4';
+arr[4] = '5';
 console.log(arr);
-
-let arr1 = ['1', '2', '3'];
-arr1[0] += 3;
-arr1[1] += 3;
-arr1[2] += 3;
-console.log(arr1);
-
-let arr3 = [1, 2, 3];
-arr3[0]++;
-arr3[1]++;
-arr3[2]++;
-console.log(arr3);
