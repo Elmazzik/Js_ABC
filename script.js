830191936a85b51fcb2f44a3599e4f80227b071b
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-15 08:19:45 +0300

    Массивы в JavaScript

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-let r = 5, Pi = 3.14, S; //Задание №1
-alert(S = r * Pi);
+let arr = [1, 2, 3];
+console.log(arr);
 
-let a = 5, S1; //Задание №2
-alert(S1 = a ** 2);
-
-let a1 = 5, b1 = 3, S2; //Задание №3
-alert(S2 = a1 * b1);
-
-let a2 = 5, b2 = 3, P; //Задание №4
-alert(S2 = (a2 * 2) + (b1 * 2));
-
-let tc = 36, F; //Задание №5
-alert(F = tc * 9 / 5 + 32);
+let arr1 = ['a', 'b', 'c'];
+console.log(arr1);
