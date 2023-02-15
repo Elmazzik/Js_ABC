6e30a0ef93ce18a5912616e2b0dc17b3fa333f7e
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-15 10:59:55 +0300

    Изменение элементов объектов в JavaScript

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-let obj = {
-	'1a': 1,
-	'b2': 2,
-	'с-с': 3,
-	'd 4': 4,
-	'e5': 5
-};
-
-console.log(obj['1a']);
-console.log(obj.b2);
-console.log(obj['c-c']);
-console.log(obj['d 4']);
-console.log(obj.e5);
+let obj = {x: 1, y: 2, z: 3};
+obj.x = obj.x**2;
+obj.y = obj.y**2;
+obj.z = obj.z**2;
+console.log(obj);
