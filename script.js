7287286f562adb1db875df4b8783265eac731f34
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-15 10:48:51 +0300

    Ограничения на ключи объектов в JavaScript

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-let obj = {year: '2023', month: 'fabruary', day: '15'};
-console.log(obj['year'] + '-' + obj['month'] + '-'+ obj['day']);
+let obj = {
+	'1a': 1,
+	'b2': 2,
+	'с-с': 3,
+	'd 4': 4,
+	'e5': 5
+};
+
+console.log(obj['1a']);
+console.log(obj.b2);
+console.log(obj['c-c']);
+console.log(obj['d 4']);
+console.log(obj.e5);
