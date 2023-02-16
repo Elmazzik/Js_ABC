986a46ad851c01e1901afce1e3b86330abfba88a
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-16 23:23:02 +0300

    Оператор in в JavaScript

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-let key = 'c';
-let obj = {
-	[key]: 1,
-	a: 2,
-	b: 3
-};
+let obj = {x: 1, y: 2, z: 3};
 
-let obj1 = {
-	x: 1,
-	y: 2,
-	z: 3
-};
-let [key1] = 'x';
-let [key2] = 'y';
-let [key3] = 'z';
+console.log('x' in obj);
+console.log('w' in obj);
