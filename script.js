0a023b2568014255133cde550f4d39f00ac48c8a
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-16 23:14:54 +0300

    Ошибка обращения к элементу по ключу в JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 let obj = {x: 1, y: 2, z: 3};
-let key = 'x';
-console.log(obj[key]);
+let x = obj.x;
+console.log(obj[x]);
+
+let obj1 = {x: 1, y: 2, z: 3};
+let key1 = 'x';
+console.log(obj1[key1]);
