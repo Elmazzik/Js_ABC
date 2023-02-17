1454151e3b09804ad19584b8545a375a5ed76217
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-17 20:13:31 +0300

    Группировка условий в JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 let num = 3;
-if (num == 0 || num > 1 && num < 5 ) {
+		
+if ((num > 5 && num < 10) || num == 20) {
 	console.log('+++');
 } else {
 	console.log('---');
-} //если num равно 0 ИЛИ больше 1 и меньше 5
+}
+
+let num1 = 3;
+		
+if (num1 > 5 || (num1 > 0 && num1 < 3)) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let num2 = 3;
+		
+if (num2 == 9 || (num2 > 10 && num2 < 20) || (num2 > 20 && num2 < 30)) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
