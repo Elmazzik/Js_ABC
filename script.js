097b1eefec1aedae742e987750b5222fcb7b3585
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-17 19:07:30 +0300

    Равенство по значению и типу в JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-let test1 = '123';
-let test2 = 123;
-if (test1['123'] == test2) {
-	console.log(' равны'); 
+let test1 = '3';
+let test2 = '3';	
+if (test1 == test2) {
+	console.log('+++');
 } else {
-	console.log(' не равны');
+	console.log('---');
+}
+
+let test3 = 3;
+let test4 = '3';
+if (test3 === test4) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let test5 = 3;
+let test6 = 3;	
+if (test5 === test6) {
+	console.log('+++');
+} else {
+	console.log('---');
 }
