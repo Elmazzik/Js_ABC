be683f8ac968acf7b1bd3de5dee10df63570b080
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-17 18:59:01 +0300

    Равенство строк в JavaScript

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-let test1 = 1;
-let test2 = 2;
-if (test2 > test1) {
-	console.log('test2 больше'); 
-} else {
-	console.log('test1 больше');
-}
+let test = 'abc';
 
-let test3 = 1;
-let test4 = 2;
-
-if (test3 == test4) {
+if (test == 'abc') {
 	console.log('переменные равны'); 
 } else {
 	console.log('переменные не равны');
