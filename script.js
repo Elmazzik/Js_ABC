60843d85d9ba8c79519087c5d6e8a43d9aa7b59c
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-17 20:10:36 +0300

    Приоритет логических операторов в JavaScript

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-let num1 = -10;
-let num2 = -10;
-if (num1 >= 0 || num2 >= 0) {
+let num = 3;
+if (num == 0 || num > 1 && num < 5 ) {
 	console.log('+++');
 } else {
 	console.log('---');
-} 
-
-let num3 = 0;
-let num4 = 0;
-if (num3 >= 0 || num4 >= 0) {
-	console.log('+++');
-} else {
-	console.log('---');
-} 
-
-let num = 1;
-if (num == 0 || num == 1) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
+} //если num равно 0 ИЛИ больше 1 и меньше 5
