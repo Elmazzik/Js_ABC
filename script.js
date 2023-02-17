d14cbdb8ff646da94b4ba568ec2fcdc1f938548f
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-17 18:57:44 +0300

    Сравнение переменных в JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let test = 1;
-if (test != 10) {
-	console.log('+++'); 
+let test1 = 1;
+let test2 = 2;
+if (test2 > test1) {
+	console.log('test2 больше'); 
 } else {
-	console.log('---');
+	console.log('test1 больше');
+}
+
+let test3 = 1;
+let test4 = 2;
+
+if (test3 == test4) {
+	console.log('переменные равны'); 
+} else {
+	console.log('переменные не равны');
 }
