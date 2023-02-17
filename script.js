0576ad980e683be75219283229589b3304b40258
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-17 20:08:30 +0300

    Логическое ИЛИ в JavaScript

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-let num = 3;
-if (num > 5 && num < 0) {
+let num1 = -10;
+let num2 = -10;
+if (num1 >= 0 || num2 >= 0) {
 	console.log('+++');
 } else {
 	console.log('---');
-}
+} 
 
-let num1 = 3;
-if (num1 >= 20 && num1 <= 10) {
+let num3 = 0;
+let num4 = 0;
+if (num3 >= 0 || num4 >= 0) {
 	console.log('+++');
 } else {
 	console.log('---');
-}
+} 
 
-let num2 = 3;
-let num3 = 3;
-if (num2 <= 1 && num3 >= 3) {
+let num = 1;
+if (num == 0 || num == 1) {
 	console.log('+++');
 } else {
 	console.log('---');
