ee2a2b445001148ffe2202863f2c26f5e7582175
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-17 18:53:24 +0300

    Проверка на равенство в JavaScript

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
 let test = 1;
-if (test > 10) {
-	console.log('+++'); 
-} else {
-	console.log('---');
-}
-
-let test1 = 1;
-if (test1 < 10) {
-	console.log('+++'); 
-} else {
-	console.log('---');
-}
-
-let test2 = 1;
-if (test2 <= 10) {
-	console.log('+++'); 
-} else {
-	console.log('---');
-}
-
-let test3 = 1;
-if (test3 >= 10) {
+if (test == 10) {
 	console.log('+++'); 
 } else {
 	console.log('---');
