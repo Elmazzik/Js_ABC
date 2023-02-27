039963a58cc26328de50f012d9aafae2ade2dee6
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-27 22:45:05 +0300

    Сокращенная форма проверки на ложь в JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 let test = true;
-
-if (test) {
+if (!test) {
 	console.log('+++');
 } else {
 	console.log('---');
 }
+
+let test1 = true;
+if (!test1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let test2 = true;
+if (test2) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
