35456e207fde44a448ac2f54e5cf0b477a7b7dfe
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-27 22:52:37 +0300

    Конструкция else if в JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-if (test > 0) 
-	console.log('+++');
-else 
-	console.log('---');
+let day = 12;
 
-	
-if (test > 0)
-	console.log('+++');
+if (day <= 10){
+    console.log("находится в первой декаде")
+} else if (day >= 11 && day <= 20){
+    console.log("находится во второй декаде")
+} else if (day >= 21 && day<=31){
+    console.log("находится в третьей декаде")
+} else {
+    console.log("число выходит за диапазон дозволенного")
+}
