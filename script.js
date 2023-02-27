ed26881d7ba61ba6aea84880a94da1479500c33c
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-27 22:30:12 +0300

    Сравнение значений с булевым типом в JavaScript

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-let test = true;
-if (test === true) {
+let test = 1;
+if (test == true) {
 	console.log('+++');
 } else {
 	console.log('---');
 }
 
-let test1 = false;
-if (test1 === false) {
+let test1 = 0;
+if (test1 == true) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let test2 = 1;
+if (test2 != true) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let test3 = '';
+if (test3 == false) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let test4;
+if (test4 == true) {
 	console.log('+++');
 } else {
 	console.log('---');
