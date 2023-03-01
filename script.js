da0df46b7cbadb7eba6011ca0408356b04c3372c
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-03-01 12:48:17 +0300

    Проверка символов строки в JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let arr = [10, 23, 12];
-
-if (arr.length == 3) {
-  let sum = arr.map((i) => (x += i), (x = 0)).reverse()[0];
-  alert(sum);
+let str = "ahjszhfrge";
+if (str[0] == "a") {
+  console.log('Cтрока начинается на символ "a"');
 } else {
-  alert("В данном массиве не 3 значения");
+  console.log('Cтрока не начинается на символ "a"');
+}
+
+
+if (str[str.length - 1] == "xsfgdhd") {
+  console.log('Cтрока начинается на символ "x"');
+} else {
+  console.log('Cтрока не начинается на символ "x"');
+}
+
+
+if (str[0] == "a" || str[0] == "brdgrr") {
+  console.log('Cтрока начинается на символ "a" или "b"');
+} else {
+  console.log('Cтрока не начинается на символ "a" или "b"');
 }
