e8d73d4d65550c793ad38ad3bce7a505f8b61472
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-03-01 12:43:22 +0300

    Проверка частей часа через if-else в JavaScript

--- script.js
+++ script.js
@@ -1,49 +1,13 @@
-let age = 17;
-let adult;
+let min = 10;
 
-if (age >= 18) {
-	adult = true;
-} else {
-	adult = false;
+if (min >= 0 && min <= 19) {
+  console.log("Четверть 1");
 }
-console.log(adult);
 
-let age2 = 17;
-let adult2;
-
-if (age2 >= 18) {
-	adult2 = true;
-} else {
-	adult2 = false;
+if (min >= 20 && min <= 39) {
+  console.log("Четверть 2");
 }
-console.log(adult2);
 
-
-let age3 = 17;
-let res;
-
-if (age3 >= 18) {
-	if (age3 <= 23) {
-		res = 'от 18 до 23';
-	} else {
-		res = 'больше 23';
-	}
-} else {
-	res = 'меньше 18';
+if (min >= 40 && min <= 59) {
+  console.log("Четверть 3");
 }
-console.log(res);
-
-
-let age4 = 19;
-let res2;
-
-if (age4 >= 18) {
-	if (age4 <= 23) {
-		res2 = 'От 18 до 23';
-	} else {
-		res2 = 'Больше 23';
-	}
-} else { 
-	res2 = 'кисяляля топ и меньше 18';
-}
-console.log(res2);
