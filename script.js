9b5f63c8bb432c75febfa73b1f551035e41d4b11
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-03-01 08:38:57 +0300

    Логические операции в JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let num = 1;
-let res = (num >= 0) ? '1' : '2';
+let a = 2 * (3 - 1);
+let b = 6 - 2;
+console.log(a == b);
 
-console.log(res);
+a1 = 5 * (7 - 4);
+b1 = 1 + 2 + 7;
+console.log(a1 > b1);
+
+a2 = 2 ** 4;
+b2 = 4 ** 2;
+console.log(a2 != b2);
