6836ae04a50c06b815dd886029b2171bc3dd87f4
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-03-01 12:53:34 +0300

    Поиск ошибок в коде с условиями JavaScript

--- script.js
+++ script.js
@@ -1,14 +1,124 @@
-let num = 120;
+//Задание 1
+let num1 = 1;
+let num2 = 2;
 
-if (num % 2 == 0) {
-	console.log('Четное число');
+if (num1 + num2 === 3) {
+  console.log("+++");
 } else {
-	console.log('Нечетное число');
+  console.log("---");
 }
 
+//Задание 2
+let num1 = "1";
+let num2 = "2";
 
-if (num % 3 == 0) {
-	console.log('Число кратно 3');
+if (+num1 + +num2 === 3) {
+  console.log("+++");
 } else {
-	console.log('Число не кратно 3');
+  console.log("---");
+}
+
+//Задание 3
+let num1 = "1";
+let num2 = "2";
+
+if (Number(num1) + Number(num2) === 3) {
+  console.log("+++");
+} else {
+  console.log("---");
+}
+
+//Задание 4
+let num = 123;
+
+if (String(num)[0] === "1") {
+  console.log("+++");
+} else {
+  console.log("---");
+}
+
+//Задание 5
+let num = 123;
+
+if (String(num)[0] === "1") {
+  console.log("+++");
+} else {
+  console.log("---");
+}
+
+//Задание 6
+let num = 123;
+
+if (String(num)[0] === "1") {
+  console.log("+++");
+} else {
+  console.log("---");
+}
+
+//Задание 7
+let num = 123;
+let first = String(num)[0];
+
+if (first === "1") {
+  console.log("+++");
+} else {
+  console.log("---");
+}
+
+//Задание 8
+let num = 12;
+
+if (String(num).length === 2) {
+  console.log("+++");
+} else {
+  console.log("---");
+}
+
+//Задание 9
+let num = 12;
+let str = String(num);
+
+if (str.length === 2) {
+  console.log("+++");
+} else {
+  console.log("---");
+}
+
+//Задание 10
+let num = 12;
+
+if (String(num).length == 2) {
+  console.log("+++");
+} else {
+  console.log("---");
+}
+
+//Задание 11
+let num = 12;
+
+if (String(num).length === 2) {
+  console.log("+++");
+} else {
+  console.log("---");
+}
+
+//Задание 12
+let num = 12;
+
+if (String(num).length === 2) {
+  console.log("+++");
+} else {
+  console.log("---");
+}
+
+//Задание 13
+let num = "123033"; 
+
+let sum1 = +num[0] + +num[1] + +num[2];
+let sum2 = +num[3] + +num[4] + +num[5];
+
+if (sum1 == sum2) {
+  console.log("суммы равны");
+} else {
+  console.log("суммы не равны");
 }
