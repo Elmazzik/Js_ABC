2ce4ba679617bd69ff003e7ceb349f696b2d9398
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-03-01 08:41:47 +0300

    Функция confirm в JavaScript

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-let a = 2 * (3 - 1);
-let b = 6 - 2;
-console.log(a == b);
+let ok = confirm("Вам есть 18?");
 
-a1 = 5 * (7 - 4);
-b1 = 1 + 2 + 7;
-console.log(a1 > b1);
-
-a2 = 2 ** 4;
-b2 = 4 ** 2;
-console.log(a2 != b2);
+if (ok) {
+  alert("Есть доступ!");
+} else {
+  alert("Доступа нет!");
+}
