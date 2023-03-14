25f68374cdec9f966fe4cdd4170a07cd2d79acc8
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-03-14 20:10:08 +0300

    Изменение массива в цикле JavaScript

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-let arr = [];
-for (let i = 1; i <= 10; i++) {
-    arr.push(i);
-}
-console.log(arr);
-
-
-
-let arr1 = [];
-for (let i = 1; i <= 10; i++) {
-    arr1.push('x');
+let arr1 = [1, 2, 3, 4, 5];
+for (let i = 0; i <= arr1.length - 1; i++) {
+    arr1[i] = arr1[i] ** 2;
 }
 console.log(arr1);
 
 
 
-let arr2 = [21, 49, -10, 15, 64];
-let arr3 = [];
-for (let i = 0; i <= arr3.length - 1; i++) {
-    if (arr2[i] >= 0) {
-        arr3.push(arr2[i]);
-    }
+let arr2 = [5, 6, 7, 8, 9];
+for (let i = 0; i <= arr2.length - 1; i++) {
+    arr2[i] -= 1;
 }
 console.log(arr2);
+
+
+
+let arr3 = [1, 2, 3, 4, 5];
+for (let i = 0; i <= arr3.length - 1; i++) {
+    arr3[i] += 10;
+}
+console.log(arr3);
