6dbfdf19bc848dc3e2eab7c14d477d2b4f43992e
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-03-14 20:03:40 +0300

    Вложенные циклы в JavaScript

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-for (let elem of arr) {
-	let result;
-	
-	if (elem % 2 == 0) {
-		result = elem * elem;
-	} else if (elem % 3 == 0) {
-		result = elem * elem * elem;
-	} else {
-		continue; // перейдем на новую итерацию цикла
+for (let i = 1; i <= 9; i++) {
+	for (let j = 1; j <= 3; j++) {
+		document.write(i);
 	}
-	
-	console.log(result); // выполнится, если делится на 2 или 3
 }
+
+
+for (let i = 1; i <= 3; i++) {
+  for (let j = 1; j <= 3; j++) {
+     document.write('\n' + i + j + ' ');
+  };
+};
