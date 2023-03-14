e6c780a9ce86399796e35a0ad57dfe3ce3233c13
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-03-14 20:12:17 +0300

    Заполнение объектов через цикл в JavaScript~

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-let arr1 = [1, 2, 3, 4, 5];
+let arr1 = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+let arr2 = [1, 2, 3, 4, 5, 6, 7];
+let obj1 = {};
 for (let i = 0; i <= arr1.length - 1; i++) {
-    arr1[i] = arr1[i] ** 2;
+    obj1[arr1[i]] = arr2[i];
 }
-console.log(arr1);
+console.log(obj1);
 
 
 
-let arr2 = [5, 6, 7, 8, 9];
-for (let i = 0; i <= arr2.length - 1; i++) {
-    arr2[i] -= 1;
+let obj21 = {a: 1, b: 2, c: 3, d: 4, e: 5};
+let obj22 = {};
+for (let i in obj21) {
+    if (obj21[i] % 2 == 0) {
+        obj22[i] = obj21[i];
+    }
 }
-console.log(arr2);
+console.log(obj22);
 
 
 
-let arr3 = [1, 2, 3, 4, 5];
-for (let i = 0; i <= arr3.length - 1; i++) {
-    arr3[i] += 10;
+let obj31 = {a: 1, b: 2, c: 3, d: 4, e: 5};
+let obj32 = {};
+for (let i in obj31) {
+    obj32[obj31[i]] = i;
 }
-console.log(arr3);
+console.log(obj32);
