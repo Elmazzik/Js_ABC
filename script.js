c4d6753298eabcbe0d39a36edf24f89198b20475
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-03-14 20:07:57 +0300

    Заполнение массива в JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-for (let i = 0; i <= 9; i++) {
-	let num = 3;
-	
-	for (let j = 0; j <= 9; j++) {
-		console.log(num); // выведет 3
-	}
+let arr = [];
+for (let i = 1; i <= 10; i++) {
+    arr.push(i);
 }
+console.log(arr);
+
+
+
+let arr1 = [];
+for (let i = 1; i <= 10; i++) {
+    arr1.push('x');
+}
+console.log(arr1);
+
+
+
+let arr2 = [21, 49, -10, 15, 64];
+let arr3 = [];
+for (let i = 0; i <= arr3.length - 1; i++) {
+    if (arr2[i] >= 0) {
+        arr3.push(arr2[i]);
+    }
+}
+console.log(arr2);
