b9d1af9dcb3f7cd88f6632f6446acfe49d81e59c
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-03-14 19:25:00 +0300

    Накопление чисел в циклах JavaScript

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-let arr = [2, 5, 9, 15, 1, 4];
-for (let elem of arr) {
-	if ((elem[i] > 3) && (elem[i] < 10)) {
-		console.log(elem);
-	}
-}
-
-let obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
-for (let key in obj) {
-  if (obj[key] % 2 == 0) {
-     console.log(obj[key]);
+let res = 0;
+for (let i = 2; i <= 100; i++)
+  if (i % 2 == 0) {
+    res = res + i
+    console.log(elem);
   }
+
+
+let res = 0;
+for (let i = 1; i <= 99; i++)
+  if (i % 2 === 0) {
+    res = res + i
+    console.log(elem);
+  }
+
+
+let res = 0;
+for (let i = 1; i <= 20; i++) {
+	res = res * i;
 }
+console.log(res);
