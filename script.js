e46c4878630fc039b387d3262353a7f23ba9fd72
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-03-14 20:36:34 +0300

    Объект с количеством элементов в массиве JavaScript

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-let arr = [1, 2, 3, 2, 4, 3, 5, 6, 3, 2, 3];
-let counter1 = 0;
-let counter2 = 0;
-for (let elem of arr) {
-	if (elem == '2') {
-		counter1++;
-	}
-  if (elem == '3') {
-		counter2++;
-	}
-}
+let str = "12225534114";
+let count = {};
 
-console.log(counter1, counter2);
+for (let i of str) {
+    if (count[i] == undefined) {
+        count[i] = 1;
+    } else {
+        count[i] += 1;
+    }
+}
+console.log(count);
