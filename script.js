19ee300e023d953eb8ea7bdfe655a675686bf4f9
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-03-15 22:22:44 +0300

    Заполнение многомерных массивов по порядку в JavaScript

--- script.js
+++ script.js
@@ -1,58 +1,139 @@
 let arr = [];
-   for (let i = 0; i < 3; i++) {
-      arr[i] = [];
-      for (let j = 1; j <= 5; j++) {
-         arr[i].push(j);
-      }
+for (let i = 0, k = 2; i < 3; i++) {
+   arr[i] = [];
+   for (let j = 0; j < 3; j++) {
+      arr[i].push(k++);
    }
-   console.log(arr);
+}
+console.log(arr);
 
 
-   let arr1 = [];
-   for (let i = 0; i < 3; i++) {
-      arr1[i] = [];
-      for (let j = 1; j <= 5; j++) {
-         arr1[i].push(j);
-      }
+let arr2 = [];
+let k = 0;
+for (let i = 0; i < 4; i++) {
+   arr2[i] = [];
+   for (let j = 0; j < 3; j++) {
+      k += 2
+      arr2[i].push(k);
    }
-   console.log(arr1);
+}
+console.log(arr2);
 
 
-   let arr2 = [];
-   for (let i = 0; i < 3; i++) {
-      arr2[i] = [];
-      for (let j = 1; j <= 5; j++) {
-         arr2[i].push(j);
-      }
+let arr3 = [];
+for (let i = 0, p = 1; i < 2; i++) {
+   arr3[i] = []
+   for (let j = 0; j < 2; j++) {
+      arr3[i][j] = [];
+      for (let k = 0; k < 2; k++) {
+         arr3[i][j].push(p++);
+      };
+   };
+};
+console.log(arr3);
+
+
+let arr4 = [];
+for (let i = 0, k = 1; i < 3; i++) {
+   arr4[i] = [];
+   for (let j = 0; j < 3; j++) {
+      arr4[i].push(k);
+      k++;
    }
-   console.log(arr2);
+}
+console.log(arr4);
 
 
-   let arr4 = [];
-   for (let i = 0; i < 3; i++) {
-      arr4[i] = [];
-      for (let j = 1; j <= 5; j++) {
-         arr4[i].push(j);
-      }
+let arr5 = [];
+let k5 = 1;
+for (let i = 0; i < 3; i++) {
+   arr5[i] = [];
+   for (let j = 0; j < 3; j++) {
+      arr5[i].push(k5++);
    }
-   console.log(arr4);
+}
+console.log(arr5);
 
 
-   let arr5 = [];
-   for (let i = 0; i < 3; i++) {
-      arr5[i] = [];
-      for (let j = 1; j <= 5; j++) {
-         arr5[i].push(j);
-      }
+let arr6 = [];
+let k6 = 1;
+for (let i = 0; i < 3; i++) {
+   arr6[i] = [];
+   for (let j = 0; j < 3; j++) {
+      arr6[i][j] = k6;
+      k6++;
    }
-   console.log(arr5);
+}
+console.log(arr6);
 
 
-   let arr6 = [];
-   for (let i = 0; i < 3; i++) {
-      arr6[i] = [];
-      for (let j = 1; j <= 5; j++) {
-         arr6[i].push(j);
-      }
+let arr7 = [];
+let k7 = 1;
+for (let i = 0; i < 3; i++) {
+   arr7[i] = [];
+   for (let j = 0; j < 3; j++) {
+      arr7[i].push(k7++)
    }
-   console.log(arr6);
+}
+console.log(arr7);
+
+
+let arr8 = [];
+for (let i = 0, k = 1; i < 3; i++) {
+   arr8[i] = [];
+   for (let j = 0; j < 3; j++) {
+      arr8[i].push(k++)
+   }
+}
+console.log(arr8);
+
+
+let arr9 = [];
+for (let i = 0, k = 1; i < 3; i++) {
+   arr9[i] = [];
+   for (let j = 0; j < 3; j++) {
+      arr9[i].push(k++)
+   }
+}
+console.log(arr9);
+
+
+let arr10 = [];
+for (let i = 0, k = 1; i < 3; i++) {
+   arr10[i] = [];
+   for (let j = 0; j < 3; j++) {
+      arr10[i].push(k++)
+   }
+}
+console.log(arr10);
+
+
+let arr11 = [];
+for (let i = 0, k = 1; i < 3; i++) {
+   arr11[i] = [];
+   for (let j = 0; j < 3; j++) {
+      arr11[i].push(k++)
+   }
+}
+console.log(arr11);
+
+
+let arr12 = [];
+let k12 = 1;
+for (let i = 0; i < 3; i++) {
+   arr12[i] = [];
+   for (let j = 0; j < 3; j++) {
+      arr12[i].push(k12++)
+   }
+}
+console.log(arr12);
+
+
+let arr13 = [];
+for (let i = 0, k = 1; i < 3; i++) {
+   arr13[i] = [];
+   for (let j = 0; j < 3; j++) {
+      arr13[i].push(k++)
+   }
+}
+console.log(arr13);
