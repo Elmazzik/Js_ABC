d90a46a9b23beea719fa4c2525a0690eefb8be36
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-06-17 23:41:54 +0300

    Поиск ошибок в коде с формами в JavaScript

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 window.onload = function () {
-   const select = document.querySelector("select");
-   for (let i = 0; i < select.options.length; i++) {
-      const option = select.options[i];
-      if (option.selected) {
-         option.textContent += "!";
-      } else {
-         option.textContent += "?";
-      }
-   }
+   let textarea = document.querySelector("textarea");
+   let div = document.querySelector("div");
 
-   const mySelect = document.getElementById("mySelect");
-   const myButton = document.getElementById("myButton");
-
-   myButton.addEventListener("click", () => {
-      mySelect.selectedIndex = mySelect.options.length - 1;
+   textarea.addEventListener("input", function () {
+      div.textContent = textarea.value;
    });
 
-   const select1 = document.getElementById("mySelect");
-   const button = document.getElementById("btn");
+   let checkbox = document.querySelector("#checkbox");
+   let button = document.querySelector("#button");
+   let div1 = document.querySelector("#div");
 
    button.addEventListener("click", function () {
-      const selectedOption = select1.options[select1.selectedIndex];
-      alert(selectedOption.textContent);
+      if (checkbox.checked) {
+         div1.textContent = "111";
+      } else {
+         div1.textContent = "222";
+      }
    });
 
-   let select2 = document.getElementById("mySelect");
-   let button1 = document.getElementById("myButton");
+   let checkbox1 = document.querySelector('[type="checkbox"]');
+   let button1 = document.querySelector("button");
 
    button1.addEventListener("click", function () {
-      let selectedOption = select2.options[select2.selectedIndex];
-      selectedOption.textContent += "!";
+      if (checkbox1.checked) {
+         console.log("+++");
+      } else {
+         console.log("---");
+      }
    });
 };
