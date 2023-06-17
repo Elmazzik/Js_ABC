972eb04b9a4ce3517ab4e86d3b89c4fd1f396ab5
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-06-17 18:09:56 +0300

    Автоматическая корректировка дат в JavaScript

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-let now = new Date();
+let date1 = new Date(2018, 1, 35);
+console.log(date1);
 
-let date = new Date(2000, 8, 31, 23, 59, 59);
-let date2 = new Date(2010, 2, 14, 23, 59, 59);
-let diff = date2 - date;
-console.log(diff);
+let date2 = new Date(2018, 15, 1);
+console.log(date2);
 
-console.log(diff / (1000 * 3600 * 24));
+let date3 = new Date(2018, 3, 31);
+console.log(date3);
 
-console.log(diff / (1000 * 60 * 60 * 24 * 31));
+let date4 = new Date(2018, 1, 31);
+console.log(date4);
 
-console.log(diff / (1000 * 60 * 60 * 24 * 31 * 12));
+let date5 = new Date(2018, 12, 33);
+console.log(date5);
+
+let date6 = new Date(2018, 33, 33);
+console.log(date6);
+
+let date7 = new Date(2018, 5, 0);
+console.log(date7);
+
+let date8 = new Date(2018, 0, 0);
+console.log(date8);
+
+let date9 = new Date(2018, -12, -33);
+console.log(date9);
+
+let date10 = new Date(2018, 0, 1, 23, 0, 60);
+console.log(date10);
+
+let date11 = new Date(2018, 0, 1, 23, 60, 0);
+console.log(date11);
+
+let date12 = new Date(2018, 0, 1, 100, 100, 100);
+console.log(date12); 
