50ca45d8208281392aac47f1bb7a4b178f57e8f0
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-06-17 21:30:33 +0300

    Другие полезные методы для поиска на JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-let parent = document.querySelector('#elem');
-let text = parent.firstElementChild.innerHTML;
+let elem = document.getElementById('elem');
+elem.textContent = 'text'
 
-console.log(text);
+
+
+
+
+let elems = document.getElementsByTagName('li');
+
+for (let elem of elems) {
+     elem.textContent += '!'
+}
+
+
+
+let elems2 = document.getElementsByClassName('www');
+
+for (let elem of elems2) {
+     elem.innerHTML = '!!!';
+}
