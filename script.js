da024f16d867cad5f2ad71712241a67f9dd5d3f1
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-06-17 21:11:33 +0300

    Объект this в JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-document.querySelector('#image').src = 'favicon.ico';
+let input = document.querySelector('#input');
 
+input.addEventListener('focus', funcFocus);
 
-let img2 = document.querySelector('#image2');
-img2.src = 'favicon.ico'
-img2.height = '100'
-img2.width = '200'
+function funcFocus() {
+ this.value = '1';
+};
+
+input.addEventListener('blur', funcBlur);
+
+function funcBlur() {
+ this.value = '2';
+};
+
+   
+let button = document.querySelector("#btn");
+
+button.addEventListener("click", addOne);
+
+function addOne() {
+ let sum = (Number(this.textContent) + 1 )
+ this.textContent = sum
+}
