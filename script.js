85bac24c5283a37cfc99306a79f755cbf12521d5
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-06-17 17:58:38 +0300

    Остаток массива при деструктуризации в JavaScript

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-let arr = [2025, 12];
-let [year, month, day] = arr;
-console.log(year);
-console.log(month);
-console.log(day);
+let arr = ["John", "Smit", "development", "programmer", 2000];
 
+let [name, surname, ...info] = arr;
 
-let arr1 = [2025, 12, 31, 23, 59, 59];
-let [year1, month1, day1] = arr1;
-
-console.log(yea1r);
-console.log(month1);
-console.log(day1); 
+console.log(name)
+console.log(surname)
+console.log(info)
