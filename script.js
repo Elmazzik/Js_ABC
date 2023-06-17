5eb3d57783c3e6b649d8d698046112163fb90133
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-06-17 17:25:30 +0300

    Оператор spread и слияние массивов

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-function func1(num1, num2, num3, num4, num5) {
-   return num1 + num2 + num3 + num4 + num5;
-}
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
 
-let arr = [1, 2, 3, 4, 5];
-console.log(func1(...arr));
+let arr = ["a", ...arr1, "b", "c", ...arr2];
+console.log(arr);
 
+let arr21 = [1, 2, 3];
+let arr22 = [4, 5, 6];
 
-function func2(n1, n2, n3, n4, n5, n6, n7, n8) {
-   return (n1 + n2 + n3 + n4) * (n5 + n6 + n7 + n8);
-}
+let arr23 = ["a", ...arr21, ...arr22, "b", "c"];
+console.log(arr23);
 
-console.log(func2(1, ...[2, 3, 4], 5, ...[6], ...[7, 8])); // 260
+let arr31 = [1, 2, 3];
+let arr32 = [...arr31, 4, 5, 6];
 
-let arr3 = [1, 2, 3, 4, 5];
-let min = Math.min(...arr3);
+let arr33 = ["a", "b", "c", ...arr32];
+console.log(arr33);
 
-console.log(min)
+let arr41 = [1, 2, 3];
+let arr42 = [...arr41, 4, 5, 6];
+let arr43 = [...arr42, 7, 8, 9];
+
+let arr44 = [0, ...arr43];
+console.log(arr44);
+
+let arr51 = [1, 2, 3];
+let arr52 = [...arr1];
+
+console.log(arr52);
