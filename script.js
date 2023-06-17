22b7511f041304280c9fe14ce175d403136aaa0e
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-06-17 17:57:48 +0300

    Лишние значения при деструктуризации массива в JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let arr = ["Anna", "Monter", "development", "programmer", 1999];
+let arr = [2025, 12];
+let [year, month, day] = arr;
+console.log(year);
+console.log(month);
+console.log(day);
 
-let [, , department, position,] = arr;
 
-console.log(department)
-console.log(position)
+let arr1 = [2025, 12, 31, 23, 59, 59];
+let [year1, month1, day1] = arr1;
+
+console.log(yea1r);
+console.log(month1);
+console.log(day1); 
