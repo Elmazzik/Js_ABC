c240f1184f4e1cf286735a963c7f72805c0027c2
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-06-17 18:05:22 +0300

    Форматирование даты в JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let date = new Date();
 
-console.log(`Текущий день: ${date.getDate()}`)
+function addZero(num) {
+   if (num >= 0 && num <= 9) {
+      return "0" + num;
+   } else {
+      return num;
+   }
+}
 
-console.log(`Текущий месяц: ${date.getMonth() + 1}`)
-
-console.log(`Текущий год: ${date.getFullYear()}`)
+console.log(
+   addZero(date.getHours()) + ':' + addZero(date.getMinutes()) + ':' + addZero(date.getSeconds()) + ' ' +
+   addZero(date.getDate()) + '.' + addZero(date.getMonth() + 1) + '.' + date.getFullYear());
