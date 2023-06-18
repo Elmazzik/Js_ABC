2299779833480e0500f973be6d54eb015e60f2d6
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-06-18 19:34:11 +0300

    Потеря контекста в JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Контекст непривязанной функции в JavaScript
+window.onload = function () {
+   // "use strict";
+
+   let elem = document.querySelector("#elem");
+   elem.addEventListener("blur", parent);
+
+   function parent() {
+      console.log(this.value); // text
+
+      function child() {
+         console.log(this.value); // undefined
+      }
+      child();
+   }
+};
