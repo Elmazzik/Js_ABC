d6f69db873b35287e39155209a4645d729c1135e
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-06-18 19:32:41 +0300

    Основы работы с контекстом в JavaScript

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-window.onload = function () {
-   list.addEventListener("click", function (event) {
-      event.target.innerHTML = event.target.innerHTML + "!";
-   });
-   // 2
-   list.addEventListener("click", function (event) {
-      let li = event.target.closest("li");
-
-      if (li) {
-         li.innerHTML = li.innerHTML + "!";
-      }
-   });
-};
+// Основы работы с контекстом в JavaScript
