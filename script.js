369f4364306c24ce2b268b017c081eb59fde551f
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-06-18 22:58:16 +0300

    Экспорт переменных и функций в модулях через замыкания в JavaScript

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-(function (root) {
-   let parent = document.querySelector(root);
+(function () {
+   let str1 = "переменная модуля";
+   let str2 = "переменная модуля";
+   let str3 = "переменная модуля";
 
-   let div1 = parent.querySelector("#div1");
-   let div2 = parent.querySelector("#div2");
-   let div3 = parent.querySelector("#div3");
+   function func1() {
+      alert("функция модуля");
+   }
+   function func2() {
+      alert("функция модуля");
+   }
+   function func3() {
+      alert("функция модуля");
+   }
 
-   let res = parent.querySelector("#res");
-   let btn = parent.querySelector("#btn");
-
-   btn.addEventListener("click", function () {
-      let num1 = Number(div1.textContent);
-      let num2 = Number(div2.textContent);
-      let num3 = Number(div3.textContent);
-
-      res.textContent = num1 + num2 + num3;
-   });
-})("#parent");
+   window.str1 = str1;
+   window.func2 = func2;
+   window.func3 = func3;
+})();
+console.log(str1); // выведет "переменная модуля"
+func2(); // вызовет функцию func2 внутри модуля
+func3(); // вызовет функцию func3 внутри модуля
